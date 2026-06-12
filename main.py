6adbcc282f824a26dd758d4be7ddd65a3d39d317
Resolushh <resolushhh@gmail.com>
2026-06-12 05:27:36 +0300

    feat: Todo CLI v1.0.0 - add, list, done, remove

---
+++ main.py
@@ -0,0 +1,34 @@
+# main.py
+import sys
+
+from todo.commands import add_task, list_tasks, mark_done, remove_task
+
+
+def main():
+    if len(sys.argv) < 2:
+        print(
+            "Usage: todo <add|list|done|remove> [args]\n"
+            "  python main.py add 'title'\n"
+            "  python main.py list\n"
+            "  python main.py done ID\n"
+            "  python main.py remove ID"
+        )
+        return
+    cmd = sys.argv[1]
+    if cmd == "add":
+        if len(sys.argv) < 3:
+            print("Need title: python main.py add 'купить хлеб'")
+            return
+        add_task(sys.argv[2])
+    elif cmd == "list":
+        list_tasks()
+    elif cmd == "done":
+        mark_done(int(sys.argv[2]))
+    elif cmd == "remove":
+        remove_task(int(sys.argv[2]))
+    else:
+        print(f"Unknown command: {cmd}")
+
+
+if __name__ == "__main__":
+    main()
